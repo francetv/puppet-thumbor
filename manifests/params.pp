295ddb0f0bab8b59b2ab50ba23c96bb7b9fa369e
btl <bertrand.thill@francetv.fr>
2019-02-08 11:35:25 +0100

    fix

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,30 +1,17 @@
-# == Class: memcached::params
+# == Class: thumbor::params
 #
 class thumbor::params {
   case $::osfamily {
     'Debian': {
-      case $install_method {
-        'pip': {
-          $security_key='MY_SECURE_KEY'
-          $port='8888'
-          $ip='0.0.0.0'
-          $config = {}
-          $conffile = '/dev/null'
-          $install_method = 'pip'
-        }
-        default: {
-          $security_key='MY_SECURE_KEY'
-          $port='8888'
-          $ip='0.0.0.0'
-          $config = {}
-          $conffile = '/dev/null'
-          $install_method = 'apt'
-        }
-      }
+      $security_key='MY_SECURE_KEY'
+      $port='8888'
+      $ip='0.0.0.0'
+      $config = {}
+      $conffile = '/dev/null'
+      $install_method = 'pip'
     }
     default: {
       fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
     }
   }
 }
-
