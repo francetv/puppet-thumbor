4db1766da613a3262d5f9817a03f398393eb5e7a
btl <bertrand.thill@francetv.fr>
2019-02-08 14:21:42 +0100

    cleaning debug

--- manifests/install/pip.pp
+++ manifests/install/pip.pp
@@ -1,15 +1,4 @@
 class thumbor::install::pip {
-
-
-
-
-
-
-
-
-
-
-
   package { ['libwebp-dev',
     'python-statsd',
     'python-crypto',
@@ -21,57 +10,46 @@ class thumbor::install::pip {
     'libmemcached-dev',
     'libcurl4-openssl-dev'
   ]: }
-
   -> package { 'colour':
     provider => 'pip'
   }
-
   -> package { 'thumbor':
     provider => 'pip',
     ensure => '5.2.1'
   }
-  
   -> package { 'tc_redis':
     provider => 'pip',
   }
-
   -> package { 'opencv-engine':
     provider => 'pip',
   }
-
   #if $::service_provider == 'systemd'{
-    thumbor::systemd{ thumbor: }
-    exec { "Reload systemd  thumbor" :
-      command => '/bin/systemctl daemon-reload',
-      before => Exec["Enable Thumbor systemd"]
-    }
-    exec { "Enable Thumbor systemd" :
-      command => "/bin/systemctl enable thumbor",
-      onlyif  => "/bin/systemctl is-enabled thumbor | /bin/grep 'disabled'",
-      #require => Class['thumbor::systemd'],
-      before  => Service["thumbor"],
-    }
- # }
-
-
-
+  thumbor::systemd{ thumbor: }
+  exec { "Reload systemd  thumbor" :
+    command => '/bin/systemctl daemon-reload',
+    before => Exec["Enable Thumbor systemd"]
+  }
+  exec { "Enable Thumbor systemd" :
+    command => "/bin/systemctl enable thumbor",
+    onlyif  => "/bin/systemctl is-enabled thumbor | /bin/grep 'disabled'",
+    #require => Class['thumbor::systemd'],
+    before  => Service["thumbor"],
+  }
+  # }
   group { 'thumbor':
     system => true,
     ensure => present
   }
-
   user { 'thumbor':
     system => true,
     gid => 'thumbor',
     home => '/var/lib/thumbor',
     require => Group['thumbor']
   }
-
   file { '/var/lib/thumbor':
     ensure => directory,
     owner => 'thumbor',
     group => 'thumbor',
     mode => '0755'
   }
- 
 }
