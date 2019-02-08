9935b802dac994ba2c68a44aac56a9262211886c
btl <bertrand.thill@francetv.fr>
2019-02-08 14:21:43 +0100

    cleaning debug

--- manifests/service.pp
+++ manifests/service.pp
@@ -11,32 +11,29 @@
 # == Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-
 case $thumbor_service_provider {
-    'systemd': {
-      file { '/etc/systemd/system/thumbor-service.cfg':
-        ensure  => $newrelic_php_service_ensure,
-        content => template('thumbor/thumbor.service.erb'),
-      }~>
-      exec { 'thumbor-systemd-reload':
-        command     => 'systemctl daemon-reload',
-        path        => [ '/usr/bin', '/bin', '/usr/sbin', '/usr/local/bin/' ],
-        refreshonly => true,
-      }
+  'systemd': {
+    file { '/etc/systemd/system/thumbor-service.cfg':
+      #ensure  => $newrelic_php_service_ensure,
+      content => template('thumbor/thumbor.service.erb'),
+    }~>
+    exec { 'thumbor-systemd-reload':
+      command     => 'systemctl daemon-reload',
+      path        => [ '/usr/bin', '/bin', '/usr/sbin', '/usr/local/bin/' ],
+      refreshonly => true,
     }
-    default: {}
   }
-
-
+  default: {}
+}
 class thumbor::service {
   service {"thumbor":
     require => File['/etc/thumbor.conf',
-                    '/etc/thumbor.key',
-                    '/etc/default/thumbor'],
+    '/etc/thumbor.key',
+    '/etc/default/thumbor'],
     ensure  => running,
     enable  => true,
     subscribe => File['/etc/thumbor.conf',
-                    '/etc/thumbor.key',
-                    '/etc/default/thumbor'],
+    '/etc/thumbor.key',
+    '/etc/default/thumbor'],
   }
 }
