7dc113ac192e322a8b503474799bbe03a113486d
btl <bertrand.thill@francetv.fr>
2019-02-08 14:21:43 +0100

    cleaning debug

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,31 +11,28 @@
 # == Sample Usage:
 #
 # [Remember: No empty lines between comments and class definition]
-class thumbor ( 
-      $security_key='MY_SECURE_KEY',
-      $port='8888',
-      $ip='0.0.0.0',
-      $config = {},
-      $conffile = '/dev/null',
-      $install_method = 'pip'
+class thumbor (
+$security_key='MY_SECURE_KEY',
+$port='8888',
+$ip='0.0.0.0',
+$config = {},
+$conffile = '/dev/null',
+$install_method = 'pip'
 ) {
-    
-    ## Modules
-    include thumbor::config
-    include thumbor::service
-
-    ## Ordering
-    Class['thumbor::config']
-    ~> Class['thumbor::service']
-
-    if $install_method == 'apt' {
-        include thumbor::repo
-        include thumbor::install::apt
-        Class['thumbor::repo'] -> Class['thumbor::install::apt'] -> Class['thumbor::config']
-    } elsif $install_method == 'pip' {
-        include thumbor::install::pip
-        Class['thumbor::install::pip'] -> Class['thumbor::config']
-    } else {
-        fail("install_method must be 'apt' or 'pip'")
-    }
+  ## Modules
+  include thumbor::config
+  include thumbor::service
+  ## Ordering
+  Class['thumbor::config']
+  ~> Class['thumbor::service']
+  if $install_method == 'apt' {
+    include thumbor::repo
+    include thumbor::install::apt
+    Class['thumbor::repo'] -> Class['thumbor::install::apt'] -> Class['thumbor::config']
+  } elsif $install_method == 'pip' {
+    include thumbor::install::pip
+    Class['thumbor::install::pip'] -> Class['thumbor::config']
+  } else {
+    fail("install_method must be 'apt' or 'pip'")
+  }
 }
