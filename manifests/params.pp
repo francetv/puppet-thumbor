2b52a5c179ce0ebf07cd35332dafe635b81359a1
btl <bertrand.thill@francetv.fr>
2019-02-08 11:29:01 +0100

    fix syntax

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,19 +5,19 @@ class thumbor::params {
     'Debian': {
       case $install_method {
         'pip': {
-          $security_key='MY_SECURE_KEY',
-          $port='8888',
-          $ip='0.0.0.0',
-          $config = {},
-          $conffile = '/dev/null',
+          $security_key='MY_SECURE_KEY'
+          $port='8888'
+          $ip='0.0.0.0'
+          $config = {}
+          $conffile = '/dev/null'
           $install_method = 'pip'
         }
         default: {
-          $security_key='MY_SECURE_KEY',
-          $port='8888',
-          $ip='0.0.0.0',
-          $config = {},
-          $conffile = '/dev/null',
+          $security_key='MY_SECURE_KEY'
+          $port='8888'
+          $ip='0.0.0.0'
+          $config = {}
+          $conffile = '/dev/null'
           $install_method = 'apt'
         }
       }
