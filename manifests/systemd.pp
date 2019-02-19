fc1cc17ed8af7d16bca748cae8f922c8627c29af
btl <bertrand.thill@francetv.fr>
2019-02-19 13:02:46 +0100

    debug

--- manifests/systemd.pp
+++ manifests/systemd.pp
@@ -1,19 +1,21 @@
 class thumbor::systemd {
+  $lastport = $thumbor::ports[-1]
   if $::service_provider == 'systemd'{
     exec { "Reload systemd  thumbor" :
       command => '/bin/systemctl daemon-reload',
-      before => Exec["Enable Thumbor systemd"]
+      before => Exec["Enable Thumbor systemd ${lastport}"]
     }
-    exec { "Enable Thumbor systemd" :
-      $thumbor::ports.each |$inst| {
-        command => "/bin/systemctl enable thumbor-${inst]",
-        onlyif  => "/bin/systemctl is-enabled thumbor-${inst} | /bin/grep 'disabled'",
-        #require => Class['thumbor::systemd'],
-        #before  => Service["thumbor"],
+    $thumbor::ports.each |$inst| {
+      exec { "Enable Thumbor systemd ${inst}" :
+        $thumbor::ports.each |$inst| {
+          command => "/bin/systemctl enable thumbor-${inst}",
+          onlyif  => "/bin/systemctl is-enabled thumbor-${inst} | /bin/grep 'disabled'",
+          #require => Class['thumbor::systemd'],
+          #before  => Service["thumbor"],
+        }
       }
     }
+    else {
+      fail("require systemd")
+    }
   }
-  else {
-    fail("require systemd")
-  }
-}
