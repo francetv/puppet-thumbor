3752e06bb43585a3c2c182ef59b35c69516efee8
btl <bertrand.thill@francetv.fr>
2019-02-19 11:30:37 +0100

    dead end

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,7 +13,7 @@
 # [Remember: No empty lines between comments and class definition]
 class thumbor (
 $security_key='MY_SECURE_KEY',
-$ports=['8888', '8889', '8890'],
+$ports=['8888','8889','8890'],
 $ip='0.0.0.0',
 $config = {},
 $conffile = '/dev/null',
@@ -21,18 +21,20 @@ $install_method = 'pip'
 ) {
   ## Modules
   include thumbor::config
-  include thumbor::service
+  #include thumbor::service
   ## Ordering
-  Class['thumbor::config']
-  ~> Class['thumbor::service']
+  #Class['thumbor::config']
+  #~> Class['thumbor::service']
   if $install_method == 'apt' {
     include thumbor::repo
     include thumbor::install::apt
     Class['thumbor::repo'] -> Class['thumbor::install::apt'] -> Class['thumbor::config']
   } elsif $install_method == 'pip' {
     include thumbor::install::pip
-    Class['thumbor::install::pip'] -> Class['thumbor::config']
+    Class['thumbor::install::pip'] -> Class['thumbor::config']-> Class['thumbor::systemd']
   } else {
     fail("install_method must be 'apt' or 'pip'")
   }
+
+
 }
