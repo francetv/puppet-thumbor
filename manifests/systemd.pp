d225722fd4b7dcbae1e87ada02d2960ea61c77fa
btl <bertrand.thill@francetv.fr>
2019-02-22 08:45:43 +0100

    add lib opencv

--- manifests/systemd.pp
+++ manifests/systemd.pp
@@ -1,9 +1,10 @@
 class thumbor::systemd {
   $lastport = $thumbor::ports[-1]
   if $::service_provider == 'systemd'{
-    exec { "Reload systemd  thumbor" :
+    exec { "Reload systemd thumbor" :
       command => '/bin/systemctl daemon-reload',
-      before => Exec["Enable Thumbor systemd ${lastport}"]
+      require => Exec["Enable Thumbor systemd ${lastport}"]
+      #before => Exec["Enable Thumbor systemd ${lastport}"]
     }
     $thumbor::ports.each |$inst| {
       exec { "Enable Thumbor systemd ${inst}" :
@@ -12,7 +13,14 @@ class thumbor::systemd {
         #require => Class['thumbor::systemd'],
         #before  => Service["thumbor"],
       }
+      exec { "start Thumbor systemd ${inst}" :
+        command => "/bin/systemctl restart thumbor-${inst}",
+        #onlyif  => "/bin/systemctl is-enabled thumbor-${inst} | /bin/grep 'disabled'",
+        require => Exec["Reload systemd thumbor"]
+        #before  => Service["thumbor"],
+      }
     }
+
   }
   else {
     fail("require systemd")
