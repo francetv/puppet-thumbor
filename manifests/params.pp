c3c33dd45195d39862b112bd00e7712a4d9d622e
btl <bertrand.thill@francetv.fr>
2021-02-04 18:34:01 +0100

    fix choice install deb10

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,12 +9,12 @@ class thumbor::params {
       $path_log='/var/log/thumbor'
       $config = {}
       $conffile = '/dev/null'
-      if versioncmp($::operatingsystemrelease, '10') >= 0 {
-        $install_method = 'pip3'
-      } else {
-        $install_method = 'pip'
-      }
-    }
+#      if versioncmp($::operatingsystemrelease, '10') >= 0 {
+#        $install_method = 'pip3'
+#      } else {
+#        $install_method = 'pip'
+#      }
+#    }
     default: {
       fail("Unsupported platform: ${::osfamily}/${::operatingsystem}")
     }
